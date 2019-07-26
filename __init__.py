3dfff6ca50382f1d8efa43e043cb0ec21c5992b8
gabaaa <not-set>
2019-07-26 17:20:48 -0500

    Bump Release Version 2.0.1

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 2.0.0
+# version: 2.0.1
 # name: gitframe
 # license: MIT
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 from .git_helpers import git_utils as git
 from .git_helpers import msg_helpers as msgh
